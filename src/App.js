55bfa741c9171a1455605b369a30f9cc85c08a49
Dhaesha M <dhaesha1@gmail.com>
2022-03-28 23:14:51 -0400

    set canvas tag in AppJs + basic css for react-three inside index.css

--- src/App.js
+++ src/App.js
@@ -2,24 +2,7 @@ import { useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Canvas></Canvas>;
 }
 
 export default App;
