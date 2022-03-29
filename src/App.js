1786761138043d91e69d9ab877bbca208c0014c6
Dhaesha M <dhaesha1@gmail.com>
2022-03-28 23:19:53 -0400

    setting cube component to have mesh tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
+
+function Cube(props) {
+  const mesh = useRef();
+  return <Mesh></Mesh>;
+}
 
 function App() {
   return (
