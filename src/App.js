c3efc114b3de814bc5b397f044939d521dacc415
Dhaesha M <dhaesha1@gmail.com>
2022-03-29 00:08:06 -0400

    added click interactivity by using const useState + pasing onClick + scale conditional functions inside mesh opening tag. it works :D

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ function Cube(props) {
   const mesh = useRef();
 
   useFrame(() => (mesh.current.rotation.x += 0.01));
-
   const [hover, setHover] = useState(false);
+  // adding click setstate
+  const [click, setClick] = useState(false);
 
   return (
     <mesh
       ref={mesh}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
+      // click logic to start false or not actiive/ clicked
+      onClick={(event) => setClick(!click)}
+      // if its active scale up n vice versa
+      scale={click ? 1.5 : 1}
     >
       <boxGeometry args={[2, 2, 2]} />
       {/* if hover is tru=> hotpink, if falsey=> pink */}
