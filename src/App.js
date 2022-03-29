36f539c86f24ef59b65b0082c0a44e06f45b06aa
Dhaesha M <dhaesha1@gmail.com>
2022-03-29 06:18:10 -0400

    vurrent style has both cubes still stacked ontop of one another

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function Cube(props) {
 
   useFrame(() => (mesh.current.rotation.x += 0.01));
   const [hover, setHover] = useState(false);
-  // adding click setstate
   const [click, setClick] = useState(false);
 
   return (
@@ -15,13 +14,10 @@ function Cube(props) {
       ref={mesh}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
-      // click logic to start false or not actiive/ clicked
       onClick={(event) => setClick(!click)}
-      // if its active scale up n vice versa
       scale={click ? 1.5 : 1}
     >
       <boxGeometry args={[2, 2, 2]} />
-      {/* if hover is tru=> hotpink, if falsey=> pink */}
       <meshStandardMaterial color={hover ? "hotpink" : "pink"} />
     </mesh>
   );
@@ -32,7 +28,8 @@ function App() {
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Cube />
+      <Cube position={[-1.2, 0, 0]} />
+      <Cube position={[1.2, 0, 0]} />
     </Canvas>
   );
 }
